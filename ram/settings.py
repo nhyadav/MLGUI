38f0540c42a682584cc6154fc57b7b0c0cabb8bf
narayan <42175861+nhyadav@users.noreply.github.com>
2022-12-04 23:39:27 +0530

    try to deploy..

--- ram/settings.py
+++ ram/settings.py
@@ -23,9 +23,9 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-dg&0@7zrj-l69(84fx4d$5qu*vn-d=-%61o&gbu&3#801lj*br'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['mlwgui.herokuapp.com','127.0.0.1']
 
 
 # Application definition
